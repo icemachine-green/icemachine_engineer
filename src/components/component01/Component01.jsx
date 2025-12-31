3eb6232c84c44e0bfb19e5517d6b23535484030c
kimjunghyun <john0210@hanmail.net>
2025-12-31 14:06:02 +0900

    251231 MAINPAGE 반응형까지 작업 완료, HEADER, FOOTER 일부 css 수정 작업 완료

--- src/components/component01/Component01.jsx
+++ src/components/component01/Component01.jsx
@@ -1,20 +1,45 @@
-import './Component01.css'; /*  component가 하나이지만, 헷갈림을 방지하기 위해 유저 사이트와 구조를 동일하게 가져갑니다.*/
+import "./Component01.css";
 
 const Component01 = () => {
+  return (
+    <div className="component01-container">
 
+      {/* 상단 프로필 영역 */}
+      <section className="profile-section">
+          <img
+            src="/public/icons/profile.png"
+            alt="기사프로필이미지"
+            className="profile-image-placeholder"
+          />       
+        <div className="profile-info">
+          <p className="profile-name">
+            김정현 <span>기사님</span>
+          </p>
+          <div className="profile-message">
+            <p>
+              <strong className="highlight">12월 22일 월요일</strong>입니다.<br />
+              오늘 예약은 <strong className="highlight">2건</strong> 입니다.
+            </p>
+          </div>
+        </div>
+      </section>
 
-   return (
-		<div className="component01-container">
-       <h1>메인입니다</h1>
-			 <h1>메인입니다</h1>
-			 <h1>메인입니다</h1>
-			 <h1>메인입니다</h1>
-			 <h1>메인입니다</h1>
-			 <h1>메인입니다</h1>
-			 <h1>메인입니다</h1>
-		</div>
-	 );
-}
+      {/* 예약 요약 카드 */}
+      <section className="summary-section">
+        <div className="summary-card">
+          <p className="card-title">오늘 예약</p>
+          <p className="card-count highlight">2<span>건</span></p>
+        </div>
 
+        <div className="summary-card">
+          <p className="card-title">이번 달 예약</p>
+          <p className="card-count highlight">31<span>건</span></p>
+        </div>
+      </section>
+
+    </div>
+  );
+};
 
 export default Component01;
+
