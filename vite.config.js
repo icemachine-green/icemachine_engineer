53df925c032bbb0e84680b1d1c435af38b033039
sunminn-byte <sunminn0627@gmail.com>
2026-01-06 21:01:52 +0900

    260106 vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Request 대상 서버 주소
+        changeOrigin: true, // Request Header Host 필드 값을 대상 서버 호스트로 변경
+        secure: false, // SSL 인증서 검증 무시
+        ws: true // WebSoket 프로토콜 사용
+      }
+    }
+  },
+});
